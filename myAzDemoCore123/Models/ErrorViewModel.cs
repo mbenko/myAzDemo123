87e0bd795c8252e80dd16fdacb2749fecc994c92
Mike Benkovich <mike@benko.com>
2018-06-04 09:42:49 -0500

    Add project files.

---
+++ myAzDemoCore123/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myAzDemoCore123.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
